662b0b25b73f8a77fafce3e201753cc13f150baa
Yongbin <rhdydqls1024@naver.com>
2022-06-13 11:24:46 +0900

    ⚙ : Add exception

---
+++ src/main/java/com/project/Dion/domain/user/exception/UserAlreadyExistsException.java
@@ -0,0 +1,15 @@
+package com.project.Dion.domain.user.exception;
+
+import com.project.Dion.global.exception.BusinessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.CONFLICT, reason = "사용자가 존재합니다")
+public class UserAlreadyExistsException extends BusinessException {
+
+    public static final UserAlreadyExistsException EXCEPTION = new UserAlreadyExistsException();
+
+    private UserAlreadyExistsException() {
+        super(HttpStatus.CONFLICT, "사용자가 존재합니다");
+    }
+}
