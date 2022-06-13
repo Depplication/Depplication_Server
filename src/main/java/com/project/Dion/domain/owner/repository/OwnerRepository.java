d017f07c3981bfbf44e15bbb73a9aff1bab61300
Yongbin <rhdydqls1024@naver.com>
2022-06-13 11:23:01 +0900

    ⚙ : Add respository

---
+++ src/main/java/com/project/Dion/domain/owner/repository/OwnerRepository.java
@@ -0,0 +1,7 @@
+package com.project.Dion.domain.owner.repository;
+
+import com.project.Dion.domain.owner.entity.Owner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OwnerRepository extends JpaRepository<Owner, String> {
+}
