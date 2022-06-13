d1389e9f46208db1d09eaf88355aa3cf5fc11f1e
Yongbin <rhdydqls1024@naver.com>
2022-06-13 11:19:16 +0900

    ⚙ : Add exception

---
+++ src/main/java/com/project/Dion/global/exception/BusinessException.java
@@ -0,0 +1,14 @@
+package com.project.Dion.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Getter
+public class BusinessException extends RuntimeException{
+
+    private final HttpStatus status;
+    private final String message;
+
+}
