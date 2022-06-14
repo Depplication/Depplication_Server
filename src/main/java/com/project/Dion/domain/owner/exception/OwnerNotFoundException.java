e4304440365d494abf0603b0b7048906999d30bf
Yongbin <rhdydqls1024@naver.com>
2022-06-14 15:05:59 +0900

    ⚙ : Add OwnerLoginResponse

---
+++ src/main/java/com/project/Dion/domain/owner/exception/OwnerNotFoundException.java
@@ -0,0 +1,16 @@
+package com.project.Dion.domain.owner.exception;
+
+import com.project.Dion.global.exception.BusinessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "아이디를 찾지 못하였습니다")
+public class OwnerNotFoundException extends BusinessException {
+
+    public static final OwnerNotFoundException EXCEPTION = new OwnerNotFoundException();
+
+    private OwnerNotFoundException() {
+        super(HttpStatus.NOT_FOUND, "아이디를 찾지 못하였습니다");
+    }
+
+}
