f0e65761f3641b25cedfc6783c7935da0bed309c
Yongbin <rhdydqls1024@naver.com>
2022-06-15 11:23:58 +0900

    ⚒ : UPDATE tokenService

--- src/main/java/com/project/Dion/domain/user/dto/response/UserLoginResponse.java
+++ src/main/java/com/project/Dion/domain/user/dto/response/UserLoginResponse.java
@@ -1,6 +1,9 @@
 package com.project.Dion.domain.user.dto.response;
 
+import com.project.Dion.domain.user.entity.User;
 import com.project.Dion.global.response.HttpResponse;
+import com.project.Dion.global.token.dto.TokenDataResponse;
+import com.project.Dion.global.token.dto.TokenResponse;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
@@ -8,10 +11,12 @@ import org.springframework.http.HttpStatus;
 public class UserLoginResponse<T> extends HttpResponse {
 
     private final T data;
+    private final TokenResponse<TokenDataResponse> token;
 
-    public UserLoginResponse(HttpStatus status, String msg, T data) {
+    public UserLoginResponse(HttpStatus status, String msg, T data, TokenResponse<TokenDataResponse> token) {
         super(status, msg);
         this.data = data;
+        this.token = token;
     }
 
 }
