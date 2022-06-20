c793880ce4f75b862a1517648e654c5368c30c0a
Yongbin <rhdydqls1024@naver.com>
2022-06-20 09:30:24 +0900

    ⚒ : ADD info, update, delete

--- src/main/java/com/project/Dion/domain/owner/service/OwnerService.java
+++ src/main/java/com/project/Dion/domain/owner/service/OwnerService.java
@@ -1,8 +1,9 @@
 package com.project.Dion.domain.owner.service;
 
-import com.project.Dion.domain.owner.dto.request.OwnerInfoRequestDto;
+import com.project.Dion.domain.owner.dto.request.OwnerDeleteRequestDto;
 import com.project.Dion.domain.owner.dto.request.OwnerJoinRequestDto;
 import com.project.Dion.domain.owner.dto.request.OwnerLoginRequestDto;
+import com.project.Dion.domain.owner.dto.request.OwnerUpdateRequestDto;
 import com.project.Dion.domain.owner.entity.Owner;
 
 public interface OwnerService {
@@ -11,6 +12,10 @@ public interface OwnerService {
 
     Owner ownerJoin(OwnerJoinRequestDto ownerJoinRequestDto);
 
-    Owner ownerInfo(OwnerInfoRequestDto ownerInfoRequestDto);
+    Owner ownerInfo(String token);
+
+    Owner ownerUpdate(String token, OwnerUpdateRequestDto ownerUpdateRequestDto);
+
+    void ownerDelete(String token, OwnerDeleteRequestDto ownerDeleteRequestDto);
 
 }
--- src/main/java/com/project/Dion/domain/owner/service/OwnerServiceImpl.java
+++ src/main/java/com/project/Dion/domain/owner/service/OwnerServiceImpl.java
@@ -1,23 +1,32 @@
 package com.project.Dion.domain.owner.service;
 
-import com.project.Dion.domain.owner.dto.request.OwnerInfoRequestDto;
+import com.project.Dion.domain.owner.dto.request.OwnerDeleteRequestDto;
 import com.project.Dion.domain.owner.dto.request.OwnerJoinRequestDto;
 import com.project.Dion.domain.owner.dto.request.OwnerLoginRequestDto;
+import com.project.Dion.domain.owner.dto.request.OwnerUpdateRequestDto;
 import com.project.Dion.domain.owner.entity.Owner;
 import com.project.Dion.domain.owner.exception.OwnerAlreadyExistsException;
 import com.project.Dion.domain.owner.exception.OwnerNotFoundException;
 import com.project.Dion.domain.owner.repository.OwnerRepository;
 import com.project.Dion.global.exception.PasswordWrongException;
+import com.project.Dion.global.token.component.JwtProvider;
+import com.project.Dion.global.token.service.TokenService;
+import com.project.Dion.global.utils.UpdateUtil;
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class OwnerServiceImpl implements OwnerService {
 
     private final OwnerRepository ownerRepository;
+    private final TokenService tService;
     private final PasswordEncoder passwordEncoder;
+    private final JwtProvider jwtProvider;
 
     @Override
     public Owner ownerLogin(OwnerLoginRequestDto ownerLoginRequestDto) {
@@ -55,14 +64,70 @@ public class OwnerServiceImpl implements OwnerService {
     }
 
     @Override
-    public Owner ownerInfo(OwnerInfoRequestDto ownerInfoRequestDto) {
+    public Owner ownerInfo(String token) {
 
-        if(!ownerRepository.existsById(ownerInfoRequestDto.getId())) {
+        tService.checkToken(token);
+
+        Claims claims = jwtProvider.parseJwtToken(token);
+
+        if(!ownerRepository.existsById(claims.getSubject())) {
             throw OwnerNotFoundException.EXCEPTION;
         }
 
-        Owner ownerInfo = ownerRepository.getById(ownerInfoRequestDto.getId());
+        Owner ownerInfo = ownerRepository.getReferenceById(claims.getSubject());
 
         return ownerInfo;
+    }
+
+    @Override
+    public Owner ownerUpdate(String token, OwnerUpdateRequestDto ownerUpdateRequestDto) {
+
+        tService.checkToken(token);
+
+        Claims claims = jwtProvider.parseJwtToken(token);
+
+        if(!ownerRepository.existsById(claims.getSubject())) {
+            throw OwnerNotFoundException.EXCEPTION;
+        }
+
+        Optional<Owner> ownerUpdate = ownerRepository.findById(claims.getSubject());
+        Owner owner = ownerRepository.getReferenceById(claims.getSubject());
+
+        UpdateUtil<String> update = new UpdateUtil<>();
+
+        ownerUpdate.ifPresent(m -> {
+            m = Owner.builder()
+                    .id(owner.getId())
+                    .pw(owner.getPw())
+                    .name(update.getUpdated(owner.getName(), ownerUpdateRequestDto.getName()))
+                    .address(update.getUpdated(owner.getAddress(), ownerUpdateRequestDto.getAddress()))
+                    .phone(update.getUpdated(owner.getPhone(), ownerUpdateRequestDto.getPhone()))
+                    .store(update.getUpdated(owner.getStore(), ownerUpdateRequestDto.getStore()))
+                    .build();
+
+            ownerRepository.save(m);
+        });
+
+        return owner;
+    }
+
+    @Override
+    public void ownerDelete(String token, OwnerDeleteRequestDto ownerDeleteRequestDto) {
+
+        tService.checkToken(token);
+
+        Claims claims = jwtProvider.parseJwtToken(token);
+        Owner owner = ownerRepository.getReferenceById(claims.getSubject());
+
+        Owner deleteOwner = Owner.builder()
+                .id(owner.getId())
+                .pw(owner.getPw())
+                .name(owner.getName())
+                .address(owner.getAddress())
+                .phone(owner.getPhone())
+                .store(owner.getStore())
+                .build();
+
+        ownerRepository.delete(deleteOwner);
     }
 }
