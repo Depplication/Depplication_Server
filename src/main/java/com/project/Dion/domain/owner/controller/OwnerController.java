0f84c478534bcbcfdd01cf533d8d08403dd6ce97
Yongbin <rhdydqls1024@naver.com>
2022-06-20 09:29:03 +0900

    ⚒ : ADD info, update, delete

--- src/main/java/com/project/Dion/domain/owner/controller/OwnerController.java
+++ src/main/java/com/project/Dion/domain/owner/controller/OwnerController.java
@@ -1,11 +1,10 @@
 package com.project.Dion.domain.owner.controller;
 
-import com.project.Dion.domain.owner.dto.request.OwnerInfoRequestDto;
+import com.project.Dion.domain.owner.dto.request.OwnerDeleteRequestDto;
 import com.project.Dion.domain.owner.dto.request.OwnerJoinRequestDto;
 import com.project.Dion.domain.owner.dto.request.OwnerLoginRequestDto;
-import com.project.Dion.domain.owner.dto.response.OwnerInfoResponse;
-import com.project.Dion.domain.owner.dto.response.OwnerJoinResponse;
-import com.project.Dion.domain.owner.dto.response.OwnerLoginResponse;
+import com.project.Dion.domain.owner.dto.request.OwnerUpdateRequestDto;
+import com.project.Dion.domain.owner.dto.response.*;
 import com.project.Dion.domain.owner.entity.Owner;
 import com.project.Dion.domain.owner.service.OwnerServiceImpl;
 import com.project.Dion.global.token.service.TokenService;
@@ -42,10 +41,30 @@ public class OwnerController {
 
     @RequestMapping(value = "/info", method = RequestMethod.GET)
     public OwnerInfoResponse<Owner> ownerInfo(
-            @RequestBody OwnerInfoRequestDto dto
+            @RequestHeader String authorization
     ) {
-        Owner info = service.ownerInfo(dto);
+        Owner info = service.ownerInfo(authorization);
 
         return new OwnerInfoResponse<Owner>(HttpStatus.OK, "업주정보 불러오기 성공", info);
+    }
+
+    @RequestMapping(value = "/update", method = RequestMethod.POST)
+    public OwnerUpdateResponse<Owner> ownerUpdate(
+            @RequestHeader String authorization,
+            @RequestBody OwnerUpdateRequestDto dto
+    ) {
+        Owner update = service.ownerUpdate(authorization, dto);
+
+        return new OwnerUpdateResponse<Owner>(HttpStatus.OK, "업데이트 성공", update);
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    public OwnerDeleteResponse ownerInfo(
+            @RequestHeader String authorization,
+            @RequestBody OwnerDeleteRequestDto dto
+    ) {
+        service.ownerDelete(authorization, dto);
+
+        return new OwnerDeleteResponse(HttpStatus.OK, "삭제 성공");
     }
 }
