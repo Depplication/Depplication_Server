e369e154cafce026b751eba1d916ce7ecc622b15
Yongbin <rhdydqls1024@naver.com>
2022-06-25 17:47:15 +0900

    ⚒ : Add Entity

--- src/main/java/com/project/Dion/domain/advertising/entity/Advertising.java
+++ src/main/java/com/project/Dion/domain/advertising/entity/Advertising.java
@@ -11,6 +11,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Date;
 
 @Entity
 @Getter
@@ -30,10 +31,40 @@ public class Advertising {
     @Column(nullable = false, name = "storeName")
     private String storeName;
 
+    @NotNull
+    @Column(nullable = false, name = "ownerName")
+    private String ownerName;
+
+    @NotNull
+    @Column(nullable = false, name = "registrationNumber")
+    private String registrationNumber;
+
+    @NotNull
+    @Column(nullable = false, name = "email")
+    private String ownerEmail;
+
+    @NotNull
+    @Column(nullable = false, name = "storeIntroMessage")
+    private String storeIntroMsg;
+
+    @NotNull
+    @Column(nullable = false, name = "startDate")
+    private Date startDate;
+
+    @NotNull
+    @Column(nullable = false, name = "endDate")
+    private Date endDate;
+
     @Builder
-    public Advertising(int idx, String name, String storeName) {
+    public Advertising(int idx, String name, String storeName, String ownerName, String registrationNumber, String ownerEmail, String storeIntroMsg, Date startDate, Date endDate) {
         this.idx = idx;
         this.name = name;
         this.storeName = storeName;
+        this.ownerName = ownerName;
+        this.registrationNumber = registrationNumber;
+        this.ownerEmail = ownerEmail;
+        this.storeIntroMsg = storeIntroMsg;
+        this.startDate = startDate;
+        this.endDate = endDate;
     }
 }
