29c8f191b70c2ef83c7f56d7381ac461f84fae56
Yongbin <rhdydqls1024@naver.com>
2022-06-25 16:58:16 +0900

    ⚒ : UPDATE Exception

--- src/main/java/com/project/Dion/domain/owner/exception/OwnerAlreadyExistsException.java
+++ src/main/java/com/project/Dion/domain/owner/exception/OwnerAlreadyExistsException.java
@@ -4,12 +4,12 @@ import com.project.Dion.global.exception.BusinessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.CONFLICT, reason = "업주가 존재합니다")
+@ResponseStatus(value = HttpStatus.CONFLICT)
 public class OwnerAlreadyExistsException extends BusinessException {
 
     public static final OwnerAlreadyExistsException EXCEPTION = new OwnerAlreadyExistsException();
 
     private OwnerAlreadyExistsException() {
-        super(HttpStatus.CONFLICT, "업주가 존재합니다");
+        super(HttpStatus.CONFLICT);
     }
 }
--- src/main/java/com/project/Dion/domain/owner/exception/OwnerNotFoundException.java
+++ src/main/java/com/project/Dion/domain/owner/exception/OwnerNotFoundException.java
@@ -4,13 +4,13 @@ import com.project.Dion.global.exception.BusinessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "업주 아이디를 찾지 못하였습니다")
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class OwnerNotFoundException extends BusinessException {
 
     public static final OwnerNotFoundException EXCEPTION = new OwnerNotFoundException();
 
     private OwnerNotFoundException() {
-        super(HttpStatus.NOT_FOUND, "업주 아이디를 찾지 못하였습니다");
+        super(HttpStatus.NOT_FOUND);
     }
 
 }
