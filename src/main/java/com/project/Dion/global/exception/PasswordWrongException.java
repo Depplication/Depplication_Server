1bc2d3d90ddf8bdb76c9631de6b14262f71f3c8c
Yongbin <rhdydqls1024@naver.com>
2022-06-25 17:55:22 +0900

    ⚒ : UPDATE Exception

--- src/main/java/com/project/Dion/global/exception/PasswordWrongException.java
+++ src/main/java/com/project/Dion/global/exception/PasswordWrongException.java
@@ -3,13 +3,13 @@ package com.project.Dion.global.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "비밀번호가 맞지 않습니다")
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class PasswordWrongException extends BusinessException {
 
     public static final PasswordWrongException EXCEPTION = new PasswordWrongException();
 
     private PasswordWrongException() {
-        super(HttpStatus.BAD_REQUEST, "비밀번호가 맞지 않습니다");
+        super(HttpStatus.BAD_REQUEST);
     }
 
 }
